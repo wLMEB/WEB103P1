e2730ed03767616931b2d9ba0cc5500f52cdcbae
Wanqi Li <wanli@umass.edu>
2026-03-01 22:05:06 -0500

    finished

---
+++ server/routes/libraries.js
@@ -0,0 +1,19 @@
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import libraryData from '../data/libraries.js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).json(libraryData)
+})
+
+router.get('/:libraryId', (req, res) => {
+  res.status(200).sendFile(path.resolve(__dirname, '../public/library.html'))
+})
+
+export default router
